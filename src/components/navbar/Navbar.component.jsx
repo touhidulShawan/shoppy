14d4eb659e1daaeb42c0bf5d6a87d6fa965813bd
Touhidul Shawan <touhidulshawan@gmail.com>
2020-03-14 02:22:42 +0600

    🚀 initial commit and add Navbar component

---
+++ src/components/navbar/Navbar.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Navbar.style.scss";
+
+import cartIcon from "../../icons/shopping_cart.svg";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <div className="logo__container">
+          <h1>shoppy</h1>
+        </div>
+        <ul className="nav__ul">
+          <li className="nav__item">
+            <a href="/" className="nav__link">
+              home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/" className="nav__link">
+              collection
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="/" className="nav__link">
+              sign in
+            </a>
+          </li>
+          <li>
+            <div className="cart">
+              <img src={cartIcon} className="cart--icon" alt="cart-icon" />
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
